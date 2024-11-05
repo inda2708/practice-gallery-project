f39541eed0e7ba6e0ff81c0ad87c99f295010ecb
inda2708 <indayunitadewi@gmail.com>
2024-11-05 08:02:00 +0800

    first commit

---
+++ script2.js
@@ -0,0 +1,18 @@
+/*function playAudio(id) { 
+    const audio = document.getElementById(id) ;
+    if (audio.berhenti) {
+    stopAllAudio();
+    audio.pause();
+    audio.play(); 
+    }
+    else {
+        audio.pause()
+        audio.currentTime = 0;
+    }
+}
+function stopAllAudio(){
+    document.querySelector('audio').forEach(audio => {
+        audio.pause();
+        audio.currentTime = 0;
+    });
+}*/
